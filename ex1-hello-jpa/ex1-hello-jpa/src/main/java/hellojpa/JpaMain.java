5750cfe61e3ad3251453c3135a1767ed639b2c9a
himdol <ihimchan746@gmail.com>
2023-04-05 14:13:51 +0900

    start jpa study

---
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    EntityManager entityManager = emf.createEntityManager();
+
+    EntityTransaction tx = entityManager.getTransaction();
+
+    // 영속성에 대해
+    // JPA 는 영속성이라는 메커니즘이 존재한다.
+    // db 통신 사이에 영속컨텍스트 에 저장하는 프로세스를 말한다.
+    // 영속 컨텍스트에 대해 컨트롤이 가능하다.
+    //
+
+    tx.begin();
+    try {
+      Member member = new Member();
+      member.setId(2L);
+      member.setName("helloB");
+      entityManager.persist(member);
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      entityManager.close();
+    }
+    emf.close();
+  }
+}
