9c5a4b99a4301299147385e313b940def2d34f72
Deloitte_himdol8@ob.co.kr <Deloitte_himdol8@ob.co.kr>
2023-04-07 14:27:27 +0900

    hello git

---
+++ ex2-hello-jpa/ex2-hello-jpa/src/main/java/org/japshop/Main.java
@@ -0,0 +1,29 @@
+package org.japshop;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		EntityManager entityManager = emf.createEntityManager();
+
+		EntityTransaction tx = entityManager.getTransaction();
+
+		// 영속성에 대해
+		// JPA 는 영속성이라는 메커니즘이 존재한다.
+		// db 통신 사이에 영속컨텍스트 에 저장하는 프로세스를 말한다.
+		// 영속 컨텍스트에 대해 컨트롤이 가능하다.
+		tx.begin();
+		try {
+			Member member = new Member();
+			member.setId("ID_A");
+			member.setName("C");
+			entityManager.persist(member);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			entityManager.close();
+		}
+		emf.close();
+	}
+	}
+}
