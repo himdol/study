11747350bde58a7244fafff957a6b4d51b9bc1af
himdol <ihimchan746@gmail.com>
2023-04-08 14:07:45 +0900

    member class

--- ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,9 @@ import javax.persistence.Persistence;
 public class JpaMain {
   public static void main(String[] args) {
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-    EntityManager entityManager = emf.createEntityManager();
+    EntityManager em = emf.createEntityManager();
 
-    EntityTransaction tx = entityManager.getTransaction();
+    EntityTransaction tx = em.getTransaction();
 
     // 영속성에 대해
     // JPA 는 영속성이라는 메커니즘이 존재한다.
@@ -18,15 +18,26 @@ public class JpaMain {
     // 영속 컨텍스트에 대해 컨트롤이 가능하다.
     tx.begin();
     try {
-      Member_Back member = new Member_Back();
-      member.setId("ID_A");
-      member.setName("C");
-      entityManager.persist(member);
+
+      Member member = new Member();
+      member.setName("member1");
+//      member.setTeamId(team.getId());
+      em.persist(member);
+      em.flush();
+      em.close();
+
+      //조회
+      Member findMember = em.find(Member.class, member.getId());
+      //연관관계가 없음
+//      Team findTeam = em.find(Team.class, team.getId());
+
+
+
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
     } finally {
-      entityManager.close();
+      em.close();
     }
     emf.close();
   }
